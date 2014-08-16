4db2fa58964278f0a91bc1488c795746f4c20cf5
Bruce Rich <brich@bruces-mbp.attlocal.net>
2014-08-16 09:38:28 -0500

    Initial Commit

---
+++ Out of this World/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Out of this World
+//
+//  Created by Bruce Rich on 8/16/14.
+//  Copyright (c) 2014 NoNameOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/XYZViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZViewController.h
+//  Out of this World
+//
+//  Created by Bruce Rich on 8/16/14.
+//  Copyright (c) 2014 NoNameOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZViewController : UIViewController
+
+@end
